b31f8cec738b2d001305cdf93e53e2bac9aa3b6a
Irimie Bogdan <irimie.bogdan@outlook.com>
2016-08-04 10:55:43 +0200

    Generate time series data with frequency ranging from high to low.

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -8,6 +8,7 @@ public class Client {
     public static void main(String[] args) throws IOException {
         Generator generator = new Generator();
         generator.generateFixIntervalRecurringEvents(1000, 5000);
-        generator.generateLowToHighFrequencyEvents(0, 5000, 0, 2000);
+        generator.generateLowToHighFrequencyEvents(0, 5000, 0, 5000);
+        generator.generateHighToLowFrequencyEvents(5000, 0, 0, 5000);
     }
 }
--- src/main/java/Generator.java
+++ src/main/java/Generator.java
@@ -57,4 +57,32 @@ public class Generator {
         }
     }
 
+    public void generateHighToLowFrequencyEvents(final int START_RPS, final int END_RPS, final int START_TIME, final int END_TIME) {
+        int timeInterval = END_TIME - START_TIME;
+        double rps_decrement = (START_RPS - END_RPS) / timeInterval;
+        double timeInLog = START_TIME;
+        double rps = START_RPS;
+        double timePerRequest;
+
+        Path path = Paths.get("generatedTraces/descending.log");
+        try {
+            Files.createDirectories(path.getParent());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try (BufferedWriter writer = Files.newBufferedWriter(path)) {
+            for (int i = START_TIME ; i < END_TIME; i++) {
+                timePerRequest = 1.0 / rps;
+                while (timeInLog <= i + 1) {
+                    timeInLog += timePerRequest;
+                    writer.append(0 + " " + String.format("%.5f", timeInLog) + "\n");
+                }
+                rps -= rps_decrement;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
